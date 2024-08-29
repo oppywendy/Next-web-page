46f98a99646e2812d32416164b86c1ebd62e7b68
oppywendy <matrixleooppy60@gmail.com>
2024-08-29 15:45:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+
+menu.addEventListener("click", function () {
+	menu.classList.toggle("is-active");
+	menuLinks.classList.toggle("active");
+});
